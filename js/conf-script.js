a481502eb92bf2604c31b4ac6dd2353f839416a8
Unknown <ocebeki@gmail.com>
2019-12-09 20:47:07 +0100

    smooth scrolling

--- js/conf-script.js
+++ js/conf-script.js
@@ -16,17 +16,42 @@
         secWrapper = document.querySelector('.conf-top__counting-down-sec');
 
         let countDownDate = new Date("Feb 27, 2020 09:00:00").getTime();
-
-        const docRoot = jQuery('html, body');
-
-        jQuery('a[href^="#"]').click(function () {
-            docRoot.animate({
-                top: "+=110",
-                scrollTop: jQuery(jQuery.attr(this, 'href')).offset().top
-            }, 500);
-
-            return false;
-        });
+        // Select all links with hashes
+        $('a[href*="#"]')
+            // Remove links that don't actually link to anything
+            .not('[href="#"]')
+            .not('[href="#0"]')
+            .click(function (event) {
+                // On-page links
+                if (
+                    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                    &&
+                    location.hostname == this.hostname
+                ) {
+                    // Figure out element to scroll to
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    // Does a scroll target exist?
+                    if (target.length) {
+                        // Only prevent default if animation is actually gonna happen
+                        event.preventDefault();
+                        $('html, body').animate({
+                            scrollTop: target.offset().top
+                        }, 1000, function () {
+                            // Callback after animation
+                            // Must change focus!
+                            var $target = $(target);
+                            $target.focus();
+                            if ($target.is(":focus")) { // Checking if the target was focused
+                                return false;
+                            } else {
+                                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                                $target.focus(); // Set focus again
+                            };
+                        });
+                    }
+                }
+            });
 
         if (daysWrapper) {
 
